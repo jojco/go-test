826cbf0d922aa3a823ce74c51522e0580964cc63
Jozef Janecek <jozef.janecek@asseco-ceit.com>
2024-02-21 06:57:54 +0000

    hotfix

--- test.go
+++
@@ -1,7 +0,0 @@
-package gotest2
-
-import "fmt"
-
-func Test() {
-	fmt.Println("Test")
-}
---
+++ utils/utils.go
@@ -0,0 +1,22 @@
+package jojcoutils
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func GetEnvWithDefault(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+func LogHandler(h http.Handler, logger *log.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
